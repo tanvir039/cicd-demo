730f411ae61f666dbb345ce4d84dceaa878496eb
tanvir039 <tanvir039@gmail.com>
2026-08-02 20:29:20 +0100

    trigger Terraform workflow

--- cicd-challenge-2/main.tf
+++ cicd-challenge-2/main.tf
@@ -11,15 +11,15 @@ terraform {
 
 provider "aws" {
   region                      = "us-east-1"
-  access_key                  = "test1"
-  secret_key                  = "test1"
+  access_key                  = "test"
+  secret_key                  = "test"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
 
 
   endpoints {
     ec2 = "http://localhost:4566"
-  }
+  } 
 }
 
 resource "aws_instance" "example" {
