db61af35e27e21f60a9f7c0ca51baac8702b61b3
tanvir039 <tanvir039@gmail.com>
2026-08-02 21:05:08 +0100

    workflow trigger change

--- cicd-challenge-2/main.tf
+++ cicd-challenge-2/main.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "aws" {
   region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
+  access_key                  = "test1"
+  secret_key                  = "test1"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
 
